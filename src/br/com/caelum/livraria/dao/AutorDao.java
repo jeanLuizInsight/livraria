db866e5cd942f7728fd4c994506597e9a7688a30
Jean Luiz <jl.zanatta92@yahoo.com.br>
2018-09-26 20:55:58 -0300

    Primeiro commit

---
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,29 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+
+	@Inject
+	private Banco banco;
+
+	public void salva(Autor autor) {
+		banco.save(autor);
+	}
+	
+	public List<Autor> todosAutores() {
+		return banco.listaAutores();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.banco.buscaPelaId(autorId);
+		return autor;
+	}
+	
+}
