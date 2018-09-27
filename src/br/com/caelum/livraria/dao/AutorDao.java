2ef0c0a8a7559d18a6a35b1b4c0ae3d9be19e67a
Jean Luiz <jl.zanatta92@yahoo.com.br>
2018-09-27 20:38:55 -0300

    Implementacao da integracao com JPA,  pool de conexoes e datasource.

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -3,26 +3,27 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager em;
 
 	public void salva(Autor autor) {
-		banco.save(autor);
+		em.persist(autor);
 	}
 	
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return em.createNamedQuery("select a from Autor", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
+		Autor autor = em.find(Autor.class, autorId);
 		return autor;
 	}
 	
--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -3,22 +3,23 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Livro;
 
 @Stateless
 public class LivroDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager em;
 	
 	public void salva(Livro livro) {
-		banco.save(livro);
+		em.persist(livro);
 	}
 	
 	public List<Livro> todosLivros() {
-		return banco.listaLivros();
+		return em.createNamedQuery("select l from Livro l", Livro.class).getResultList();
 	}
 	
 }
